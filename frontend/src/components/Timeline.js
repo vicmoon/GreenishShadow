e0d5d514d1d4243ac3c5713856c79a3bae23b376
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-10-14 09:34:28 +0200

    update timeline

--- frontend/src/components/Timeline.js
+++ frontend/src/components/Timeline.js
@@ -4,23 +4,60 @@ import "./Timeline.css";
 
 function Timeline() {
   const articles = [
-    { id: 1, title: "Article I", date: "01/01/24", image: "/game.jpg" },
-    { id: 2, title: "Article II", date: "01/01/24", image: "/game.jpg" },
+    {
+      id: 1,
+      title: "Article I",
+      date: "01/01/24",
+      image: "/article.jpeg",
+      type: "article",
+    },
+    {
+      id: 2,
+      title: "Game Update",
+      date: "01/01/24",
+      image: "/game.jpg",
+      type: "game update",
+    },
+    {
+      id: 3,
+      title: "Article II",
+      date: "01/01/24",
+      image: "/article.jpeg",
+      type: "article",
+    },
+    {
+      id: 4,
+      title: "Game Update",
+      date: "01/01/24",
+      image: "/game.jpg",
+      type: "game update",
+    },
   ];
 
   return (
     <div className="timeline">
-      {articles.map((article, index) => (
+      {articles.map((article) => (
         <div
-          className={`timeline-item ${index % 2 === 0 ? "left" : "right"}`}
+          className={`timeline-item ${
+            article.type === "game update" ? "right" : "left"
+          }`}
           key={article.id}
         >
           <div className="timeline-content">
             <h3>{article.title}</h3>
-            <img src={article.image} alt={article.title} />
+            <Link
+              to={`/article/${article.id}`}
+              className="timeline-article-link"
+            >
+              <img src={article.image} alt={article.title} />
+            </Link>
             <p>{article.date}</p>
-            <Link to={`/article/${article.id}`}>Read More</Link>{" "}
-            {/* Dynamic link */}
+            {/* <Link
+              to={`/article/${article.id}`}
+              className="timeline-article-link"
+            >
+              More
+            </Link> */}
           </div>
         </div>
       ))}
