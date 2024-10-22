a6b951d745e682e52d077e27384c8e173b305e7c
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-10-22 09:43:22 +0200

    remove Logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import ArticleBody from './components/ArticleBody';
 import CreatePost from './components/CreatePost';
 import Register from './components/Register';
 import Login from './components/Login';
-import Logout from './components/Logout';
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
         <Route path="/create-post" element={<CreatePost />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/login" element={<Logout />} />
       </Routes>
     </Router>
   );
